53119c633e64e43a7f70ee2690a1cac40239fd1d
Mathiyarasu18 <138567222+Mathiyarasu18@users.noreply.github.com>
2024-07-31 19:18:21 +0530

    Create Longest_substring_without_repeating_characters.java

---
+++ Longest_substring_without_repeating_characters.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.lang.Math;
+public class Main {
+	
+	public static int subString(String word) {
+		
+		char arr[] = word.toCharArray();
+		int n=arr.length;
+		if(n<2)
+			return n;
+		int left=0,right=0;
+		HashMap<Character,Integer>hm=new HashMap<Character,Integer>();
+		int maxLen = 0;
+		while(right<n) {
+			hm.put(arr[right], hm.getOrDefault(arr[right], 0)+1);
+			while(hm.size()!=right-left-1) {
+				hm.put(arr[left], hm.get(arr[left])-1);
+				if(hm.get(arr[left])==0) {
+					hm.remove(arr[left]);
+				}
+				left++;
+			}
+			maxLen = Math.max(maxLen, right-left-1);
+			right++;
+			}
+		return maxLen;
+		}
+	
+	public static void main(String args[]) {
+		
+		String str = "abcabcbb";
+		System.out.println(subString(str));
+	}
+
+}
